e4a159d0c11f8af05e125fb4003de42d27b2a001
Alexandre pocky Balmes <albalmes@gmail.com>
2010-12-02 00:35:26 +0100

    Création du projet

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    peanut
+ * @subpackage form
+ * @author     Your name here 
+ * @version    SVN: $Id$
+ */
+class BaseForm extends sfFormSymfony
+{
+}

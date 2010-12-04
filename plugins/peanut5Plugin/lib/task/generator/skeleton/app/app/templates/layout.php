2b2d88c25f38d3a35c140d32c0d37c706cc4ea75
Alexandre pocky Balmes <albalmes@gmail.com>
2010-12-04 03:06:18 +0100

    Début des modifications du layout afin de penser à IE (entre autre).

--- plugins/peanut5Plugin/lib/task/generator/skeleton/app/app/templates/layout.php
+++ plugins/peanut5Plugin/lib/task/generator/skeleton/app/app/templates/layout.php
@@ -1,20 +1,35 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+
   <head>
     <meta charset="utf-8" />
-    <?php include_http_metas() ?>
 
+    <?php include_http_metas() ?>
     <?php include_metas() ?>
 
     <?php include_title() ?>
 
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/peanut5Plugin/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" href="/peanut5Plugin/favicon/apple-touch-icon.png">
 
     <?php include_stylesheets() ?>
 
-    <?php include_javascripts() ?>
+    <?php echo javascript_include_tag('/peanut5Plugin/js/modernizr-1.6.min.js') ?>
+
   </head>
+
   <body>
-    <?php echo $sf_content ?>
+    <div id="container">
+
+      <div id="main" role="main">
+        <?php echo $sf_content ?>
+      </div>
+
+    </div>
+
+    <?php include_javascripts() ?>
   </body>
 </html>

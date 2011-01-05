896abd4d3bb9b4639217140aef6237e98515d0ec
Alexandre pocky Balmes <albalmes@gmail.com>
2011-01-05 23:50:32 +0100

    Ajout des widgets pour les input date et assimilés

---
+++ plugins/peanutHtml5Plugin/lib/widget/sfWidgetFormHtml5InputDate.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Generate an html5 input type="date"
+ *
+ * @package peanut5Plugin
+ * @subpackage widget
+ * @author Alexandre 'pocky' Balmes <albalmes@gmail.com>
+ */
+
+
+class sfWidgetFormHtml5InputDate extends sfWidgetFormHtml5InputNumber
+{
+
+  /**
+   * Constructor.
+   *
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+
+    $this->setOption('type', 'date');
+  }
+
+}
---
+++ plugins/peanutHtml5Plugin/lib/widget/sfWidgetFormHtml5InputDateTime.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Generate an html5 input type="datetime"
+ *
+ * @package peanut5Plugin
+ * @subpackage widget
+ * @author Alexandre 'pocky' Balmes <albalmes@gmail.com>
+ */
+
+
+class sfWidgetFormHtml5InputDateTime extends sfWidgetFormHtml5InputDate
+{
+
+  /**
+   * Constructor.
+   *
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+
+    $this->setOption('type', 'datetime');
+  }
+
+}

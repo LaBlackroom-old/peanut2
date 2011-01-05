7c31784dd78b0c10101e150eb62902927a5d37b7
Alexandre pocky Balmes <albalmes@gmail.com>
2011-01-05 17:04:32 +0100

    Ajout des tests pour le sfWidgetFormHtml5InputTel

--- plugins/peanutHtml5Plugin/test/unit/widget/sfWidgetFormHtml5InputTelTest.php
+++ plugins/peanutHtml5Plugin/test/unit/widget/sfWidgetFormHtml5InputTelTest.php
@@ -2,7 +2,14 @@
 
 include(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(1);
+$t = new lime_test(3);
 $w = new sfWidgetFormHtml5InputTel();
 
 $t->is($w->render('tel'), '<input type="tel" name="tel" id="tel" />', 'render tag ok');
+
+$t->comment('Simple test with options');
+$w->setOption('pattern', '[0-9]*');
+$t->is($w->render('tel'), '<input type="tel" name="tel" pattern="[0-9]*" id="tel" />', 'render pattern [0-9]*');
+
+$w->setOption('size', '10');
+$t->like($w->render('tel'), '/size="10"/', 'render size 10');

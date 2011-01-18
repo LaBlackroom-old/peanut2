097e07b1fa425baf3439d2f3ba03bb5786275aab
Alexandre pocky Balmes <albalmes@gmail.com>
2011-01-18 16:37:17 +0100

    Duplication des widget text et file de symfony pour utiliser les nouveaux attributs html5 (y'a de la FileAPI dans l'air mais je dis ça, je dis rien).

---
+++ plugins/peanutHtml5Plugin/lib/widget/sfWidgetFormHtml5InputFile.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Generate an html5 input type="file"
+ *
+ * @package peanut5Plugin
+ * @subpackage widget
+ * @author Alexandre 'pocky' Balmes <albalmes@gmail.com>
+ */
+
+
+class sfWidgetFormHtml5InputFile extends sfWidgetFormHtml5Input
+{
+
+  /**
+   * Constructor.
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+
+    $this->setOption('type', 'file');
+    $this->setOption('needs_multipart', true);
+  }
+
+}
---
+++ plugins/peanutHtml5Plugin/lib/widget/sfWidgetFormHtml5InputText.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Generate an html5 input type="text"
+ *
+ * @package peanut5Plugin
+ * @subpackage widget
+ * @author Alexandre 'pocky' Balmes <albalmes@gmail.com>
+ */
+
+
+class sfWidgetFormHtml5InputText extends sfWidgetFormHtml5Input
+{
+
+  /**
+   * Constructor.
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+
+    $this->setOption('type', 'text');
+  }
+
+}

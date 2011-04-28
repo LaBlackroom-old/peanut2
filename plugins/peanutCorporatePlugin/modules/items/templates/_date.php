3c1bc2e691014ed104e6cc6e73d8b3aaff69c2f7
Alexandre pocky Balmes <albalmes@gmail.com>
2011-04-29 00:15:59 +0200

    Supression de peanutCorporatePlugin pour le passage sur le repository

--- plugins/peanutCorporatePlugin/modules/items/templates/_date.php
+++
@@ -1,8 +0,0 @@
-<?php use_helper('Date') ?>
-
-<p>
-  <?php echo __('Published at', null, 'peanutCoporate') ?>
-  <time pubdate datetime="<?php echo format_date($created, 'yyyy-MM-dd', 'en') ?>">
-    <?php echo format_date($updated, 'dd MMMM yyyy') ?>
-  </time>
-</p>

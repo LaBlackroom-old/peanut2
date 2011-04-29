0087e3a24cf3dc8fc49c0894ca0115c391a6683f
Alexandre pocky Balmes <albalmes@gmail.com>
2011-04-29 16:52:12 +0200

    Supression de peanutCorporatePlugin pour nettoyer le repo

--- plugins/peanutCorporatePlugin/modules/items/templates/_author.php
+++
@@ -1,3 +0,0 @@
-<p itemscope itemtype="http://data-vocabulary.org/Person">
-  <?php echo __('Published by',null, 'peanutCorporate') ?> <span itemprop="name"><?php echo $author['first_name'] . ' ' . $author['last_name'] ?></span>
-</p>

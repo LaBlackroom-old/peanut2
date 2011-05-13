23756ff546efc258b6c6516a2b8a4f0e79c0e43e
Alexandre pocky Balmes <albalmes@gmail.com>
2011-05-13 18:46:54 +0200

    Un peu de wording

--- apps/admin/modules/dashboard/templates/indexSuccess.php
+++ apps/admin/modules/dashboard/templates/indexSuccess.php
@@ -5,7 +5,8 @@
     <h1><?php echo __('Hi ') . $sf_user->getGuardUser() . __('!') ?></h1>
     <p>
       <?php echo __('Today') ?>, <?php echo __('we are the') ?> <?php echo format_date(date('D'), 'dddd dd MMMM yyyy') ?>. 
-        <?php echo __('It make') ?> <?php echo $weather['temp_c'] ?>°C (<?php echo $weather['condition'] ?>).
+        <?php echo __('It make') ?> <?php echo $weather['temp_c'] ?>°C (<?php echo $weather['condition'] ?>) <?php echo __('at') ?> 
+        <?php echo sfConfig::get('app_google_city') ?>.
     </p>
   </header>
   

b488b325be7d3bd33ab1dea97075c73300730126
Alexandre pocky Balmes <albalmes@gmail.com>
2011-06-13 16:55:48 +0200

    Fix pour dd_belated

--- apps/www/templates/layout.php
+++ apps/www/templates/layout.php
@@ -67,7 +67,7 @@
     </div>
 
     <!--[if lt IE 7 ]>
-      <script src="js/libs/dd_belatedpng.js"></script>
+      <script src="js/dd_belatedpng.js"></script>
       <script>DD_belatedPNG.fix('img, .png_bg');</script>
     <![endif]-->
 

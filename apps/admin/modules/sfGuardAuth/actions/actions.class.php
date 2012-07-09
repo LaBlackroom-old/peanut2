3be4799eec59a81559ec4d53afaf753c0ed74afd
Nicolas X9 Dachez <ndachez@epsi.fr>
2012-07-09 15:25:05 +0200

    Correction changement préc.

--- apps/admin/modules/sfGuardAuth/actions/actions.class.php
+++ apps/admin/modules/sfGuardAuth/actions/actions.class.php
@@ -25,8 +25,6 @@ class sfGuardAuthActions extends BasesfGuardAuthActions
   public function executeSignin($request)
   {
     parent::executeSignin($request);
-    
-    sfContext::getInstance()->getResponse()->setCookie('peanutAdmin', '', time()+60*60*24*15, '/');
 
     $this->setLayout('login');
   }

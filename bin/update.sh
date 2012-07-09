573a6bdd8716bd1e6e1eeca97354cb418c2c52f1
Nicolas X9 Dachez <ndachez@epsi.fr>
2012-07-09 15:40:51 +0200

    Le plugin sfImageTransform trouve maintenant son helper dans lib/helper

--- bin/update.sh
+++ bin/update.sh
@@ -89,10 +89,6 @@ install_git peanutSeoPlugin git://github.com/LaBlackroom/peanutSeoPlugin.git mas
 # ndSocialPlugin
 install_git ndSocialPlugin git://github.com/LaBlackroom/ndSocialPlugin.git master
 
-# sfImageTransformPlugin
-install_git sfImageTransformPlugin git://git.rufus.lan/plugins-symfony/sfimagetransformplugin.git master
-
-
 cd ../..
 
 

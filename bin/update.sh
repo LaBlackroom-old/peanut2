80094ca2fbbc9bc5b6f0504ece8c943a88aa9fef
ndachez <ndachez@epsi.fr>
2012-09-03 12:37:30 +0300

    Ajout du sfImageTransformPlugin

--- bin/update.sh
+++ bin/update.sh
@@ -89,6 +89,10 @@ install_git peanutSeoPlugin git://github.com/LaBlackroom/peanutSeoPlugin.git mas
 # ndSocialPlugin
 install_git ndSocialPlugin git://github.com/LaBlackroom/ndSocialPlugin.git master
 
+# sfImageTransformPlugin
+install_git sfImageTransformPlugin git://github.com/ndachez/sfImageTransformPlugin.git master
+
+
 cd ../..
 
 
